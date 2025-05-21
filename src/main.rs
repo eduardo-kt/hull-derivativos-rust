c4750116510961d3e60de790bbcb4d8156f3acf7
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-05-21 08:03:35 -0300

    feat: adiciona struct Termo com implementação de Display e Debug

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct Termo {
+    strike_price: u64,
+    spot_price: u64,
+}
+
+impl Termo {
+    fn new(strike_price:u64, spot_price:u64) -> Self {
+        Self { strike_price, spot_price }
+    }
+}
+
+impl fmt::Display for Termo {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Termo(K:${:.2}, S:${:.2})",
+            self.strike_price as f64 / 100.0,
+            self.spot_price as f64 / 100.0)
+    }
+    
+}
+
+fn main() {
+    let termo = Termo::new(1300,1645);
+    println!("{:?}",termo);
+    println!("{}", termo);
+}
