3ff644a234b25f3e7c90f8c083c9924aabb51d56
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-23 11:45:46 -0300

    feat(bsm): adiciona métodos valor_esperado e desvio_padrao_esperado
    - valor esperado: implementa equação 15.4 (p. 345).
    - desvio_padrao_esperado: implementa equação 15.5 (p. 345)

--- src/bin/bsm.rs
+++ src/bin/bsm.rs
@@ -14,7 +14,7 @@ impl Ativo {
         Ativo { preco_inicial, retorno_esperado, volatilidade}
     }
 
-    fn intervalo_preco_95(self, delta_time: f64) -> Vec<f64> {
+    fn intervalo_preco_95(&self, delta_time: f64) -> Vec<f64> {
         let mut preco95: Vec<f64> = Vec::new();
         
         let s0 = self.preco_inicial.ln();
@@ -32,11 +32,26 @@ impl Ativo {
         preco95.push(upper.exp());
         preco95
     }
+    fn valor_esperado(&self, delta_time: f64) -> f64 {
+        let valor_esperado = self.preco_inicial*(self.retorno_esperado*delta_time).exp();
+        valor_esperado
+    }
+    fn desvio_padrao_esperado(self, delta_time: f64) -> f64 {
+        (self.preco_inicial.powi(2)*
+        (2.0*self.retorno_esperado*delta_time).exp()*
+        ((self.volatilidade.powi(2)*delta_time).exp()-1.0)).sqrt()
+    }
     
 }
 
 fn main() {
     let ex151 = Ativo::new(40.0, 0.16, 0.20);
-    print!("{:?}", ex151.intervalo_preco_95(0.5));
+
+    print!("{:.2?}", ex151.intervalo_preco_95(0.5));
+    
+    println!("\n");
+    let ex152 = Ativo::new(20.0, 0.2, 0.4);
+    print!("\n{:.2?}", ex152.valor_esperado(1.0));
+    print!("\n{:.2?}", ex152.desvio_padrao_esperado(1.0));
 
 }
